d7caf08aee768f3d43087b98c34b834de5f602f4
Anshumali Karna <anshumali.karna99@gmail.com>
2024-07-14 23:11:20 +0530

    New

--- client/src/app/profile/page.tsx
+++ client/src/app/profile/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
+import Sidebar from "@/components/Sidebar";
+import Widgets from "@/components/Widgets";
+import ProfileHeader from "@/components/profile/ProfileHeader";
+import ProfileTweets from "@/components/profile/ProfileTweets";
 
 const style = {
-  wrapper: `flex justify-center h-screen w-screen select-none bg-[#15202b] text-white`,
+  wrapper: `flex justify-center h-screen w-screen select-none bg-[#070910] text-white`,
   content: `max-w-[1400px] w-2/3 flex justify-between`,
   mainContent: `flex-[2] border-r border-l border-[#38444d] overflow-y-scroll`,
 };
@@ -9,7 +13,14 @@ const style = {
 const Profile = () => {
   return (
     <div className={style.wrapper}>
-      <div>profile</div>
+      <div className={style.content}>
+        <Sidebar initialSelectedIcon={"Profile"} />
+        <div className={style.mainContent}>
+          <ProfileHeader />
+          <ProfileTweets />
+        </div>
+        <Widgets />
+      </div>
     </div>
   );
 };
