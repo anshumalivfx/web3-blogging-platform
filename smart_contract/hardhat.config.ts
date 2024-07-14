7c0082c39df59db31966cefe6eba796808dffd3a
Anshumali Karna <anshumali.karna99@gmail.com>
2024-07-15 00:00:42 +0530

    smart contract 1

--- smart_contract/hardhat.config.ts
+++ smart_contract/hardhat.config.ts
@@ -1,8 +1,15 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+require('dotenv').config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    rinkeby: {
+      url: '',
+      accounts: [process.env.PRIVATE_KEY as string]
+    }
+  }
 };
 
 export default config;
